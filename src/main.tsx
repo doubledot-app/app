60527b23a6f8b4b8467cab74c33300595b7d1de5
Mahdi Farahani <mahtifarahani@gmail.com>
2024-10-08 01:15:41 +0330

    feat(root): add translation

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import { Root } from "@/components/Root"
 
 import "@telegram-apps/telegram-ui/dist/styles.css"
 import "./index.css"
-import "./translation/i18n"
+import "./translation/config"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<Root />)
