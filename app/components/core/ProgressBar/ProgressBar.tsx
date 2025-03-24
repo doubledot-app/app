942c15b826eb07dc736bdae777ac6beff92d42a2
Vigen <47vigen@gmail.com>
2025-03-24 16:47:37 +0330

    refactor: remove unused imports and commented code in ProgressBar component

--- app/components/core/ProgressBar/ProgressBar.tsx
+++ app/components/core/ProgressBar/ProgressBar.tsx
@@ -2,10 +2,6 @@ import {NProgress} from 'nprogress-v2';
 import {useEffect, useRef} from 'react';
 import {useNavigation} from 'react-router';
 
-// import {SpinnerLoading} from '@Components/ui/Loading';
-// import {cn} from '@Utils/style';
-// import {Modal as AriaModal, ModalOverlay as AriaModalOverlay} from 'react-aria-components';
-
 export const ProgressBar = () => {
   NProgress.configure({showSpinner: false});
   const timerRef = useRef<NodeJS.Timeout | null>(null);
@@ -36,21 +32,4 @@ export const ProgressBar = () => {
   }, [isNavigating]);
 
   return null;
-  // return (
-  //   <AriaModalOverlay
-  //     isOpen={isNavigating}
-  //     isDismissable={false}
-  //     className={cn(
-  //       'fixed inset-0 z-[51] flex items-center justify-center bg-white/80',
-  //       /* Exiting */
-  //       'data-[exiting]:duration-300 data-[exiting]:animate-out data-[exiting]:fade-out-0',
-  //       /* Entering */
-  //       'data-[entering]:animate-in data-[entering]:fade-in-0'
-  //     )}
-  //   >
-  //     <AriaModal>
-  //       <SpinnerLoading className="size-8 text-primary" />
-  //     </AriaModal>
-  //   </AriaModalOverlay>
-  // );
 };
