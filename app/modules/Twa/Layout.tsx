b17529da5131aac1373cdab92607f2ad949ba2bc
Vigen <47vigen@gmail.com>
2025-03-26 14:27:09 +0330

    fix: prevent rendering of TwaLayout on the server side to optimize performance

--- app/modules/Twa/Layout.tsx
+++ app/modules/Twa/Layout.tsx
@@ -1,13 +1,14 @@
 import type {PropsWithChildren} from 'react';
 
-import {lazy, Suspense} from 'react';
+import {isServer} from '@tanstack/react-query';
+import {lazy} from 'react';
 
 const TwaProvider = lazy(() => import('./components/TwaProvider'));
 
 export default function TwaLayout(props: PropsWithChildren) {
-  return (
-    <Suspense>
-      <TwaProvider>{props.children}</TwaProvider>
-    </Suspense>
-  );
+  if (isServer) {
+    return null;
+  }
+
+  return <TwaProvider>{props.children}</TwaProvider>;
 }
