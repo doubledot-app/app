74bae64f3e6c705dacbf34c086dd636c87a0681b
Vigen <47vigen@gmail.com>
2025-03-26 15:14:08 +0330

    feat: add full height styling to Placeholder and AppRoot components for improved layout

--- app/modules/Home/Page.tsx
+++ app/modules/Home/Page.tsx
@@ -3,6 +3,7 @@ import {Placeholder} from '@telegram-apps/telegram-ui';
 export default function HomePage() {
   return (
     <Placeholder
+      className="h-full"
       header={'Welcome to DoubleDot.'}
       description={'We are not ready yet, please wait.'}
     >
--- app/modules/Twa/components/TwaProvider.tsx
+++ app/modules/Twa/components/TwaProvider.tsx
@@ -7,6 +7,7 @@ import WebApp from '@twa-dev/sdk';
 export default function TwaProvider(props: PropsWithChildren) {
   return (
     <AppRoot
+      className="h-full w-full"
       appearance={WebApp.colorScheme}
       platform={['macos', 'ios'].includes(WebApp.platform) ? 'ios' : 'base'}
     >
